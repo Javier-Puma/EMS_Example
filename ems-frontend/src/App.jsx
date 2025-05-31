579b770071e6f9c7f24ea94bf0d788c91f9846f0
Sogni_Appesi <juegosxepor@gmail.com>
2025-05-31 14:40:17 -0500

    creating header and footer

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import FooterCompoment from './components/footerCompoment'
+import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
 
 function App() {
   return (
     <>
+    <HeaderComponent/>
     <ListEmployeeComponent/>
+    <FooterCompoment/>
     </>
   )
 }
---
+++ ems-frontend/src/components/FooterCompoment.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FooterCompoment = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>All rights reserved 2025 by Sogni_Appesi</span>
+
+        </footer>
+    </div>
+  )
+}
+
+export default FooterCompoment
