68e6884e28d314f0ec42ca42ff51475c41c04f31
Sogni_Appesi <juegosxepor@gmail.com>
2025-05-31 15:07:11 -0500

    configuring access routes

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -2,13 +2,19 @@ import './App.css'
 import FooterCompoment from './components/footerCompoment'
 import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
-    <HeaderComponent/>
-    <ListEmployeeComponent/>
-    <FooterCompoment/>
+      <BrowserRouter>
+        <HeaderComponent />
+        <Routes>
+          {/*<Route path='/' element={<ListEmployeeComponent />}></Route>*/}
+          <Route path='/employees' element={<ListEmployeeComponent />}></Route>
+        </Routes>
+        <FooterCompoment />
+      </BrowserRouter>
     </>
   )
 }
