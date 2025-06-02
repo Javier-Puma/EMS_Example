7383fa126ab10b9e2f3434c1196f1e03d0adc8d0
Sogni_Appesi <juegosxepor@gmail.com>
2025-06-01 20:57:50 -0500

    Modifying EmployeeComponent Functions

--- ems-frontend/src/components/EmployeeCOomponent.jsx
+++ ems-frontend/src/components/EmployeeCOomponent.jsx
@@ -4,16 +4,7 @@ const Employeecomponent = () => {
   const [firstName, setFirstName]=useState('')
   const [lastName, setLastName]=useState('')
   const [email,setEmail]=useState('')
-
-  function handleFirstName(e){
-    setFirstName(e.target.value)
-  }
-  function handleLastName(e){
-    setLastName(e.target.value)
-  }
-  function handleEmail(e){
-    setEmail(e.target.value)
-  }
+  
   function saveEmployee(e){
     e.preventDefault();
     const employee={firstName,lastName,email}
@@ -22,8 +13,9 @@ const Employeecomponent = () => {
 
     return (
     <div className='container'>
+        <br/><br/>
         <div className='row'>
-            <div className='card'>
+            <div className='card col-md-6 offset-md-3 offset-md-3'>
                 <h2 className='text-center'>Add Employee</h2>
                 <div className='card-body'>
                     <form>
@@ -35,7 +27,7 @@ const Employeecomponent = () => {
                             name='firstName'
                             value={firstName}
                             className='form-control'
-                            onChange={handleFirstName}>
+                            onChange={(e) => setFirstName(e.target.value)}>
                             </input>
                         </div>
                         <div className='form-group mb-2'>
@@ -46,7 +38,7 @@ const Employeecomponent = () => {
                             name='lastName'
                             value={lastName}
                             className='form-control'
-                            onChange={handleLastName}>
+                            onChange={(e) => setLastName(e.target.value)}>
                             </input>
                         </div>
                         <div className='form-group mb-2'>
@@ -57,7 +49,7 @@ const Employeecomponent = () => {
                             name='email'
                             value={email}
                             className='form-control'
-                            onChange={handleEmail}>
+                            onChange={(e) => setEmail(e.target.value)}>
                             </input>
                         </div>
                         <button className='btn btn-success' onClick={saveEmployee}>Submit</button>
